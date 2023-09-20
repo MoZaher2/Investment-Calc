0c39fa5c86f66822e4c417f6fd43a14270d30c8f
unknown <mohamedzahertaha@gmail.com>
2023-09-20 11:43:14 +0300

    Add Time Calculation

--- main.js
+++ main.js
@@ -9,7 +9,21 @@ form.addEventListener("submit", (e) => {
       x = +x + x * 0.05;
     }
     x = x.toFixed(2);
-    span.innerHTML = `Money after ${round.value} round will be: ${x}`;
+    span.innerHTML = `Money after ${round.value} round will be: ${x}$ `;
   }
   e.preventDefault();
+});
+// Calc Time
+let form2 = document.querySelector('[class="formtime"]');
+let time = document.querySelector('[id="time"]');
+let spanTime = document.querySelector('[id="valtime"]');
+form2.addEventListener("submit", (ele) => {
+  if (time.value != "") {
+    let h = Math.trunc(+time.value / 60);
+    let day = Math.trunc(+h / 24);
+    let hour = +h % 24;
+    let minute = +time.value % 60;
+    spanTime.innerHTML = `Round will be ended after ( ${day} Days : ${hour} Hours : ${minute} Minutes )`;
+  }
+  ele.preventDefault();
 });
